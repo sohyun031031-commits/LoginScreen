4952694f3c1f317492a452d06b39bf6e57747d1e
qwe-r-r <sohyun031031@gmail.com>
2026-04-02 11:44:39 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace LoginScreen
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAppName = new TextBox();
+            btnLogin = new Button();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.BackColor = SystemColors.Control;
+            lblAppName.BorderStyle = BorderStyle.None;
+            lblAppName.Font = new Font("맑은 고딕", 48F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblAppName.ForeColor = Color.Blue;
+            lblAppName.Location = new Point(122, 36);
+            lblAppName.Name = "lblAppName";
+            lblAppName.ReadOnly = true;
+            lblAppName.Size = new Size(177, 86);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            lblAppName.TextChanged += textBox1_TextChanged;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(192, 192, 255);
+            btnLogin.Location = new Point(166, 347);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(83, 44);
+            btnLogin.TabIndex = 1;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtID.ForeColor = Color.Silver;
+            txtID.Location = new Point(81, 172);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(257, 39);
+            txtID.TabIndex = 2;
+            txtID.Text = "아이디";
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtPW.ForeColor = Color.Silver;
+            txtPW.Location = new Point(81, 246);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(257, 39);
+            txtPW.TabIndex = 3;
+            txtPW.Text = "패스워드";
+            txtPW.TextChanged += txtPW_TextChanged;
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // LoginScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(431, 450);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(btnLogin);
+            Controls.Add(lblAppName);
+            Name = "LoginScreen";
+            Text = "Login Screen";
+            Load += LoginScreen_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox lblAppName;
+        private Button btnLogin;
+        private TextBox txtID;
+        private TextBox txtPW;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+using Microsoft.VisualBasic;
+
+namespace LoginScreen
+{
+    public partial class LoginScreen : Form
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "아이디")
+            {
+                txtID.Text = "";
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                txtID.Text = "아이디";
+                txtID.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPW_Enter(object sender, EventArgs e)
+        {
+            if (txtPW.Text == "패스워드")
+            {
+                txtPW.Text = "";
+                txtPW.ForeColor = Color.Black;
+                txtPW.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPW_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPW.Text))
+            {
+                txtPW.UseSystemPasswordChar = false;
+                txtPW.Text = "패스워드";
+                txtPW.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPW_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        string myID = "admin";
+        string myPW = "superman";
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inputID = txtID.Text;
+            string inputPW = txtPW.Text;
+
+            if (inputID == myID && inputPW == myPW)
+            {
+                MessageBox.Show("로그인 성공!");
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패~");
+            }
+        }
+    }
+}
